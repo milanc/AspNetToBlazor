762a83beca14310fd90fc8e9144ca30ef7371a89
milanc <8333458+milanc@users.noreply.github.com>
2024-03-28 09:32:13 -0400

    Tweaks for enhanced navigation.

--- WebFormsApp/Site.Master.cs
+++ WebFormsApp/Site.Master.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebFormsApp
 {
     public partial class SiteMaster : MasterPage
     {
+        public string BlazorBase { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            BlazorBase = Request.GetOwinContext().Request.Uri.GetLeftPart(UriPartial.Authority) + "/";
         }
     }
 }
