eb885993f9f22e9e0cbab3e2e49c21fe33c69bcd
milanc <8333458+milanc@users.noreply.github.com>
2024-03-28 09:32:12 -0400

    Moved NavMenu to the client project to use as custom element in webforms.

--- BlazorWebApp/BlazorWebApp.Client/Program.cs
+++ BlazorWebApp/BlazorWebApp.Client/Program.cs
@@ -1,9 +1,13 @@
 using BlazorWebApp.Client;
+using BlazorWebApp.Client.Components;
 
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.RegisterCustomElement<NavMenu>("nav-menu");
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
